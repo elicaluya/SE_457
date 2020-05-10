495c8183041948b25c6ac5ab4a08b1cb265deb70
Elijah Caluya <elicaluya@yahoo.com>
2020-05-09 23:01:18 -0700

    added starting files for HW3

---
+++ HW3/src/main/java/SE457_HW3/App.java
@@ -0,0 +1,13 @@
+package SE457_HW3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
