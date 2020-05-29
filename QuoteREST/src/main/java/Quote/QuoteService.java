4407071dab9245ed5b7199ec1701fa9fe096a103
Elijah Caluya <elicaluya@yahoo.com>
2020-05-28 22:30:04 -0700

    edited QuoteService

--- QuoteREST/src/main/java/Quote/QuoteService.java
+++ QuoteREST/src/main/java/Quote/QuoteService.java
@@ -1,51 +1,66 @@
 package Quote;
 
+import java.util.ArrayList;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.xml.bind.annotation.XmlRootElement;
 
-import com.sun.net.httpserver.HttpContext;
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
-
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
+import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
+import org.apache.cxf.jaxrs.lifecycle.SingletonResourceProvider;
 
 
-
-@Path("quotes")
+@Produces("application/json")
 public class QuoteService {
 	
-	public static void main(String[] args) throws Exception {
-		String hello = "hello";
-		HttpServer server = HttpServer.create(new InetSocketAddress(8888),0);
-		server.createContext("/quotes", new GetHandler());
-		server.setExecutor(null);
-		server.start();
+	@XmlRootElement
+	public static class QuoteObject {
+		int id;
+		String quote;
 	}
 	
-	// This method is called if TEXT_PLAIN is request
-	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public static String getIt() {
-		return "Got It!";
-	}
-
-}
-
-class GetHandler implements HttpHandler {
-
-	@Override
-	public void handle(HttpExchange he) throws IOException {
-		String hello = "hello";
-		he.sendResponseHeaders(200, hello.getBytes().length);
-		final OutputStream output = he.getResponseBody();
-		output.write(hello.getBytes());
-		output.flush();
-		he.close();
+	static ArrayList<QuoteObject> list = new ArrayList<>();
+	
+	@Path("/quotes")
+	public static class RestService{
+		
+		@GET
+		@Path("/get/{id}")
+		public QuoteObject getQuoteById(@PathParam("id") int id) {
+			QuoteObject qo = null;
+			for (QuoteObject q : list) {
+				if (q.id == id)
+					qo = q;
+			}
+			if (qo == null)
+				System.out.println("QuoteObject not found");
+			else
+				System.out.println("QuoteObject requested: " + qo.id + " " + qo.quote);
+			return qo;
+		}
 	}
 	
+	public static void main(String[] args) {
+		QuoteObject q1 = new QuoteObject(), q2 = new QuoteObject(), q3 = new QuoteObject(), q4 = new QuoteObject();
+		q1.id = 1; q1.quote = "q1";
+		q2.id = 2; q2.quote = "q2";
+		q3.id = 3; q3.quote = "q3";
+		q4.id = 4; q4.quote = "q4";
+		list.add(q1);
+		list.add(q2);
+		list.add(q3);
+		list.add(q4);
+		
+		JAXRSServerFactoryBean sf = new JAXRSServerFactoryBean();
+		sf.setResourceClasses(RestService.class);
+		sf.setResourceProvider(RestService.class, new SingletonResourceProvider(new RestService()));
+		sf.setAddress("http://localhost:8888/");
+		sf.create();
+	}
+
 }
